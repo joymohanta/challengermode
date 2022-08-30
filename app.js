033dfd63ef260e1b6212c120d585f19384478a7a
joyMohanta <joyroy1062@gmail.com>
2022-08-30 23:54:23 +0600

    something done

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("joy");
+const loadButton = document.querySelector(".show_more_button");
+let currentItem = 1;
+
+loadButton.addEventListener("click", (e) => {
+  const items = [...document.querySelectorAll(".more_item .per_item")];
+  e.target.classList.add("show-loader");
+
+  for (let i = currentItem; i < currentItem + 1; i++) {
+    setTimeout(function () {
+      e.target.classList.remove("show-loader");
+      if (items[i]) {
+        items[i].style.display = "flex";
+      }
+    }, 1000);
+  }
+  currentItem += 1;
+
+  if (currentItem >= items.length) {
+    e.target.classList.add("loaded");
+  }
+});
